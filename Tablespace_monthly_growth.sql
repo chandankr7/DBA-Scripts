f066e57aedf13a5d61dbb92fe9088aa269c59ce5
Chandan Kumar <chandankr7@outlook.com>
2017-05-12 16:33:29 +0530

    Update Tablespace_monthly_growth.sql

--- Tablespace_monthly_growth.sql
+++ Tablespace_monthly_growth.sql
@@ -1,6 +1,6 @@
 # Script to monitor database tabelspace growth in a month
 
-SELECT INSTANCE_NAME,REPORT_DATE,TS_NAME,(sum(ALLOCATED_MB)/1024) ALLOCATED_GB,(sum(USED_MB)/1024) USED_MB,
+SELECT INSTANCE_NAME,REPORT_DATE,TS_NAME,(sum(ALLOCATED_MB)/1024) ALLOCATED_GB,(sum(USED_MB)/1024) USED_GB,
 (sum(FREE_MB)/1024) FREE_GB FROM DBGRTH.TABLESPACES_GROWTH_INFO
 WHERE to_char(REPORT_DATE,'MON-YYYY')=to_char(last_day(add_months(trunc(sysdate),-1)),'MON-YYYY')
 AND trim(to_char(REPORT_DATE,'DAY'))='MONDAY' and ts_name not in('TEMP','TEMP_TBS')
