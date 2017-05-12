558a20fbe50d9e83a8b47e0984542ab3d3c81a49
Chandan Kumar <chandankr7@outlook.com>
2017-05-12 15:59:40 +0530

    Create Kill_sessions.sql

---
+++ Kill_sessions.sql
@@ -0,0 +1,32 @@
+
+#Script to generate and kill session as per session id
+
+select 'alter system kill session '''||sid||','||serial#||''' immediate;'
+from v$session
+where sid=&1
+/
+
+#Script to generate and kill session as per hash value
+
+select 'alter system kill session '''||sid||','||serial#||''' immediate;'
+from v$session
+where sql_hash_value= &a;
+
+#Script to generate and kill session as per username
+
+select 'alter system kill session '''||sid||','||serial#||''' immediate;' from v$session where USERNAME in ('&ENTER_USERNAME')
+/
+
+#Script to identify database SID as per OS PID
+
+select s.sql_hash_value, s.sql_id,s.program,s.module,s.username,s.osuser  from v$session s , v$process p where p.addr=s.paddr and p.spid=&spid
+/
+
+# Script to generate and kill sessions with long operations and in Inactive state
+
+ select 'alter system kill session'''||b.sid||','||b.serial#||''' immediate;'
+  from v$session_longops a, v$session b
+  where
+  a.sid = b.sid and
+  totalwork <> SOFAR and b.SQL_HASH_VALUE=0 and b.STATUS='INACTIVE'
+  order by start_time;
